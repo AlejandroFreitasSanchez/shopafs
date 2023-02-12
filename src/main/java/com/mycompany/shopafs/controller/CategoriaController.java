06f5d0d38dd5fc62f95dcb8593a9a46f75e7bc85
Alejandro Freitas <freitassanchezalejandro@gmail.com>
2023-02-12 22:40:46 +0100

    commit.

--- src/main/java/com/mycompany/shopafs/controller/CategoriaController.java
+++ src/main/java/com/mycompany/shopafs/controller/CategoriaController.java
@@ -19,13 +19,16 @@ import jakarta.inject.Named;
 @Named
 @ApplicationScoped
 public class CategoriaController extends AbstractController<Categoria> {
+    //introduce un objeto de tiendacontroller en la clase 
+    //(también la aplicación)
+    //no siendo necesario crear uno nuevocon “new”
     @Inject
     TiendaController tiendacontroller;
     public CategoriaController() {
         super(Categoria::new);
         //this.load();
     }
-
+    //se ejecuta al crear un objeto de la clase
     @Override
     @PostConstruct
     public void load() {
@@ -48,6 +51,7 @@ public class CategoriaController extends AbstractController<Categoria> {
         this.add();
     }
 
+    //si no hay ninguna tienda con la categoria, se elimina
     public String remove() {
         if (this.getSelected() != null) {
             if (this.tiendacontroller.getItems().stream().filter(item -> {
@@ -64,13 +68,13 @@ public class CategoriaController extends AbstractController<Categoria> {
         return "";
 
     }
-
+    //devuelve el string edit, para poder enviarlo al jsf con el formulario a partir del archivo faces-config
     @Override
     public String preEdit() {
 
         return "edit";
     }
-
+    //añade una nueva categoria
     @Override
     public String add() {
         //si es nuevo
